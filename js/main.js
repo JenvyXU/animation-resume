a10ca3639b9df6b5878b45f142d0cfd2ca1cf0c7
jenvyxu <1272117264@qq.com>
2018-08-01 17:05:56 +0800

    修改动画效果

--- js/main.js
+++ js/main.js
@@ -1,77 +1,117 @@
 var result1=`
 /**
-*你好，~我是许振威~
 *
+*面试官你好，~
 *
-*只用文字介绍我太单调了~
+*我是~许振威~
+*
+*只用静态的文字介绍我太单调了~
 *
 *所以~
 *
-*换成动画的方式来介绍我自己...~
+*决定换成动画的形式来介绍我自己...~
 *
 */
-*{
-  transition:all 1s;
-}
+
+/**
+*可是文字太靠边了，先设置好文字的位置吧~
+*/
+#code{
+    padding:10px;   
+    }
+/**
+*同样把文字和背景颜色也修改一下吧~
+*/  
 body{
   background:rgb(63, 82, 99);
-  font-size:14px;
-  color:white;
-  height:100vh;
-}               
-/*文字太靠边了，挪一下位置*~ /   
+} 
+.token.selector,
+.token.property,
+.token.function,
+.token.comment,
+.token.punctuation,
 #code{
-  padding:16px; 
+    color: white;
+}    
+#code{
+    max-height:60%;
+    width:49%; 
+    background:rgb(48, 48, 48);
+    overflow:auto; 
 }
-/*代码只有一种颜色，高亮一下~*/
+
+/**
+*嗯，代码的颜色只有一种，看起来有点单调，那就对代码高亮一下吧~
+*/~
 .token.selector{ color: #690; }
 .token.property{ color: #905; }
-/*把界面移动到左边*/
+
+/**
+*然后~
+*把界面移动到左边~
+*/
 body{
     perspective:1000px;
 }
 #code{
-    width:49%;
+    margin-left:50%;
     background:rgb(48, 48, 48);
     outline:1px solid white;
-    margin-left:50%;
     transform-origin: right;
     transform: translateY(1%) rotateY(-10deg); 
-    max-height:97%; 
+    max-height:97%;
 }
+
 
 `
 var result2=`
-/*接下来需要一个合适的位置来介绍我自己~*/
+/**
+*我想在屏幕的左边介绍我自己~
+*那么在左边准备一个窗口吧~
+*/
 #paper{
   position:fixed;
   top:1%;
   left:1%;
-  width:49%;
-  height:97%;
-  overflow:auto;
-  background:rgb(48, 48, 48);
-  outline:1px solid white;  
   transform-origin: left;
-  transform: rotateY(10deg);
+  transform: rotateY(10deg); 
+  width:49%;
+  min-height:50%;  
+  background:rgb(48, 48, 48);
+  outline:1px solid white;    
+  overflow:auto;  
 }
+
 #paper>.content{
-padding:10px;
-width:100%;
-color:white;
+    padding:10px;
+    width:100%;
+    color:white;
 }
+
+/**
+*好哒，一切都已经准备就绪~
+*接下来我会在屏幕的左边来介绍我自己~
+*/
 `
+
 var profile=`
-# 自我介绍
+# 基本信息
 
-我叫许振威
-1992年1月30日
+姓名：许振威
+性别：男
+出生年月：1992年1月30日
 毕业院校：大连海事大学(2011~2015)
-
-自学前端半年，希望应聘前端开发岗位
+专业：通信工程
+学历：本科
+### 联系方式
+Wechat:xuzhenwei111 | QQ:1272117264 | Phone:18316304477 | Email:1272117264@qq.com
+ 
+# 自我介绍
+大学毕业后一直从事计算机硬件相关的工作，因为想转行互联网，因此自学前端半年多，希望应聘前端开发岗位，
+经过半年多的摸索学习，能够熟练使用Html、CSS3和Javascript制作前端网页，100%还原设计图，熟悉jQuery
+和ES6语法，理解MVC设计模式，熟练使用Vue框架。
 
 # 技能介绍
-
 * JavaScript 熟练度：70%
 * CSS3
 * jQuery 
@@ -82,19 +122,19 @@ var profile=`
 1. 键盘导航
    技术栈：原生JS
 2. canvas画板
-   技术栈：canvas 原生js
+   技术栈：Canvas 原生js
+4. CSS3画皮卡丘
 3. 网易云音乐
    技术栈：MVC框架
-4. vue简历
+4. Vue在线简历编辑器
    技术栈：Vue
 
-# 联系方式
-QQ:1272117264@qq.com
-手机:18316304477
+
 
 `
 var result3=`
-/*接下把md格式的自我介绍转成html形式
+/**
+*接下把md格式的自我介绍转成html形式
 *3~
 *2~
 *1~
@@ -109,16 +149,16 @@ var result3=`
 `
 
 var result4=`
-/*但是页面看起来怪怪的，调整一下样式~
+/**
+*可是页面的样式不怎么好看，作一下调整吧~
 */
 #paper{
     padding:40px;
 }
-#paper>a{     color:white;}
+#paper>a{ color:white; }
 #paper>.content>ul,ol{
     list-style:none;
-   }
-   s
+    }
 #paper{    box-shadow: 0px 0px 40px 5px rgba(255,255,255,0.4);}
 #code{    box-shadow: 0px 0px 40px 5px rgba(255,255,255,0.4);}
 `
@@ -155,7 +195,7 @@ function writeCode(prefix,code,fn){
     let id=setInterval(()=>{
         if(isPlay){
             n+=1
-            if(code[n-1]==='~'){
+            if(code[n]==='~'){
                 let reg = new RegExp('~')
                 code = code.replace(reg,'')
                 console.log(code);
